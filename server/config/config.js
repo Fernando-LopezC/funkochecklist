fc3f97056e4f0f8d79ac4397eac2bfa4baaac17d
Luis Fernando <luisfernando_181818@hotmail.com>
2021-11-03 03:28:09 -0600

    Connection test

--- server/config/config.js
+++ server/config/config.js
@@ -1,23 +1,31 @@
 require('dotenv').config();
 
 const defaultConfig = {
-  username: process.env.JAWSDB_USER,
+  username: process.env.MARIADB_USER,
+  password: process.env.MARIADB_PASSWORD,
+  database: process.env.MARIADB_DATABASE,
+  host: process.env.MARIADB_HOST,
+  port: process.env.MARIADB_PORT,
+  dialect: 'mariadb' 
+};
+
+module.exports = {
+  development: defaultConfig,
+  production: Object.assign(defaultConfig, {
+    username: process.env.JAWSDB_USER,
     password: process.env.JAWSDB_PASSWORD,
     database: process.env.JAWSDB_DATABASE,
     host: process.env.JAWSDB_HOST,
-    dialect: 'mariadb'
+    dialect: 'mariadb' 
+  })
 };
+
+
 
 // const productionConfig = {
 
-//     username: process.env.JAWSDB_USER,
-//     password: process.env.JAWSDB_PASSWORD,
-//     database: process.env.JAWSDB_DATABASE,
-//     host: process.env.JAWSDB_HOST,
-//     dialect: 'mariadb'
-// }
-
-module.exports = {
-  development: defaultConfig
-  // production: productionConfig
-};
+//   username: process.env.JAWSDB_USER,
+//   password: process.env.JAWSDB_PASSWORD,
+//   database: process.env.JAWSDB_DATABASE,
+//   host: process.env.JAWSDB_HOST,
+//   dialect: 'mariadb'
