119e74d7bbba927341f5297adc9dd7338e3aa5b2
Luis Fernando <luisfernando_181818@hotmail.com>
2021-11-03 03:01:48 -0600

    Trying to fix config

--- server/config/config.js
+++ server/config/config.js
@@ -1,24 +1,23 @@
 require('dotenv').config();
 
 const defaultConfig = {
-  username: process.env.MARIADB_USER,
-  password: process.env.MARIADB_PASSWORD,
-  database: process.env.MARIADB_DATABASE,
-  host: process.env.MARIADB_HOST,
-  port: process.env.MARIADB_PORT,
-  dialect: 'mariadb' 
-};
-
-const productionConfig = {
-
-    username: process.env.JAWSDB_USER,
+  username: process.env.JAWSDB_USER,
     password: process.env.JAWSDB_PASSWORD,
     database: process.env.JAWSDB_DATABASE,
     host: process.env.JAWSDB_HOST,
     dialect: 'mariadb'
-}
+};
+
+// const productionConfig = {
+
+//     username: process.env.JAWSDB_USER,
+//     password: process.env.JAWSDB_PASSWORD,
+//     database: process.env.JAWSDB_DATABASE,
+//     host: process.env.JAWSDB_HOST,
+//     dialect: 'mariadb'
+// }
 
 module.exports = {
-  development: defaultConfig,
-  production: productionConfig
+  development: defaultConfig
+  // production: productionConfig
 };
